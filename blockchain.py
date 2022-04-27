6f02c77318f4ebdba485ee988db891e15ec348e4
ryanlui16055 <ryanlui1605@gmail.com>
2022-04-28 01:07:40 +0800

    updated function

--- blockchain.py
+++ blockchain.py
@@ -4,45 +4,46 @@ import json
 
 from numpy import block
 
+
 class blockchain(object):
     def __init__(self):
         self.chain = []
         self.pendingTransactions = []
-        self.CreateBlock(previous_hash="The Times 03/Jan/2009 Chancellor on brink of second bailout for banks.",proof=100)
+        self.CreateBlock(
+            previous_hash="First hash",
+            proof=100,
+        )
 
-    def CreateBlock(self,proof,previous_hash = None):
-        block ={
+    def CreateBlock(self, proof, previous_hash=None):
+        block = {
             # length of blockchain
-            'index': len(self.chain) + 1,
+            "index": len(self.chain) + 1,
             # timestamp when created a block
-            'timestamp': time(),
+            "timestamp": time(),
             # pending list
-            'trainsactions': self.pendingTransactions,
+            "trainsactions": self.pendingTransactions,
             # valid 'nonce'
-            'proof': proof,
+            "proof": proof,
             # the most recent approved block
-            'previous_hash' :previous_hash or self.hash(self.chain[-1])
+            "previous_hash": previous_hash or self.hash(self.chain[-1]),
         }
         self.pendingTransactions = []
         self.chain.append(block)
 
         return block
+
     @property
     def last_block(self):
 
         return self.chain[-1]
 
-    def new_transaction(self,sender,recipient,amount):
-        transaction = {
-            'sender' : sender,
-            'recipient' : recipient,
-            'amount':amount
-        }
+    def new_transaction(self, sender, recipient, amount):
+        transaction = {"sender": sender, "recipient": recipient, "amount": amount}
         self.pendingTransactions.append(transaction)
-        return self.last_block['index']+1
+        return self.last_block["index"] + 1
 
-    def hash(self,block):
-        string_object = json.dumps(block,sort_keys=True)
+    def hash(self, block):
+        string_object = json.dumps(block, sort_keys=True)
         # encode obj to binary form
         block_string = string_object.encode()
 
@@ -53,14 +54,14 @@ class blockchain(object):
 
 
 blockchain_1 = blockchain()
-t1 = blockchain_1.new_transaction("Satoshi", "Mike", '5 BTC')
-t2 = blockchain_1.new_transaction("Mike", "Satoshi", '1 BTC')
-t3 = blockchain_1.new_transaction("Satoshi", "Hal Finney", '5 BTC')
+t1 = blockchain_1.new_transaction("Satoshi", "Mike", "5 BTC")
+t2 = blockchain_1.new_transaction("Mike", "Satoshi", "1 BTC")
+t3 = blockchain_1.new_transaction("Satoshi", "Hal Finney", "5 BTC")
 blockchain_1.CreateBlock(12345)
 
-t4 = blockchain_1.new_transaction("Mike", "Alice", '1 BTC')
-t5 = blockchain_1.new_transaction("Alice", "Bob", '0.5 BTC')
-t6 = blockchain_1.new_transaction("Bob", "Mike", '0.5 BTC')
+t4 = blockchain_1.new_transaction("Mike", "Alice", "1 BTC")
+t5 = blockchain_1.new_transaction("Alice", "Bob", "0.5 BTC")
+t6 = blockchain_1.new_transaction("Bob", "Mike", "0.5 BTC")
 blockchain_1.CreateBlock(6789)
 
 print("Genesis block: ", blockchain_1.chain)
--- qr_code.py
+++ qr_code.py
@@ -1,36 +1,63 @@
 from cgitb import grey
 import cv2
+from matplotlib import image
 import numpy as np
 import pyzbar.pyzbar as pyzbar
 import imutils
+import qrcode
+from PIL import Image
+import json
 
 
 def qr_code_decoder(img_path):
     img = cv2.imread(img_path)
-    grey_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    decode_img = pyzbar.decode(grey_img)     
+    grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    decode_img = pyzbar.decode(grey_img)
     for text in decode_img:
         print(text)
 
         (x, y, w, h) = text.rect
-        draw_rectangle(x,y,w,h,img)
+        draw_rectangle(x, y, w, h, img)
 
-        if text !=[]: return text.data.decode("utf-8")
-        else: return 'Cannot decode image'
+        if text != []:
+            return text.data.decode("utf-8")
+        else:
+            return "Cannot decode image"
 
-def draw_rectangle(x,y,w,h,img):
-    
+
+def draw_rectangle(x, y, w, h, img):
+
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
-    cv2.imshow("Image", img) 
+    cv2.imshow("Image", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-# for testing photo recognizebitily, feel free to change angle to test.        
+
+# for testing photo recognizebitily, feel free to change angle to test.
 def rotate_img(img_path):
     img = cv2.imread(img_path)
-    rotated_img = imutils.rotate_bound(img,angle=30)
-    cv2.imshow("hah",rotated_img)
+    rotated_img = imutils.rotate_bound(img, angle=30)
+    cv2.imshow("hah", rotated_img)
     cv2.waitKey(0)
 
-print(qr_code_decoder(r'.\barcode2.PNG'))
-rotate_img(r'.\barcode2.PNG')
+
+# txt is qr code content, string type and it will save as image, feel free to change name in img.save("")
+def generate_QRcode(txt):
+    img = qrcode.make(txt)
+    img.save("test.png")
+    cv2.imshow("Image", r".\test.png")
+
+
+def identify_hash(provided_chain, chain_in_product):
+    for x, y, z in zip(provided_chain, chain_in_product, range(len(provided_chain))):
+        if x["previous_hash"] != y["previous_hash"]:
+            return False
+        if z == len(provided_chain):
+            break
+    return True
+
+
+print(qr_code_decoder(r".\test.png"))
+# rotate_img(r".\abc.png")
+# cv2.imshow("QR", generate_QRcode("this is me"))
+# generate_QRcode("this is me")
